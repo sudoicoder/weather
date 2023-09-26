66979ff55f8806689780f17585bef6d85f8e66cc
sudoicoder <sudoicoder@gmail.com>
2023-09-26 12:23:01 +0530

    Simplified WeatherCard

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,18 +1,29 @@
-import type { Weather } from "@/entities/Weather"
+import type { GeoLocation } from "@/entities/GeoLocation"
+import type { CurrentWeather } from "@/entities/Weather"
 
 import formatTemperature from "@/utilities/formatTemperature"
 
-export default function WeatherCard({ weather }: WeatherCardProps) {
+export default function WeatherCard({
+  currentWeather,
+  geoLocation,
+}: WeatherCardProps) {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center align-baseline">
       <span className="text-3xl font-bold">
-        {formatTemperature(weather.currentWeather.temperature)}
+        {formatTemperature(currentWeather.currentTemperature)}
       </span>
-      <span className="italic">{weather.geoLocation.name}</span>
+      <span className="italic">{geoLocation.name}</span>
+      <span className="text-sm">{currentWeather.condition}</span>
+      <div>
+        <span>{formatTemperature(currentWeather.maximumTemperature)}</span>
+        {" / "}
+        <span>{formatTemperature(currentWeather.minimumTemperature)}</span>
+      </div>
     </div>
   )
 }
 
 interface WeatherCardProps {
-  weather: Weather
+  currentWeather: CurrentWeather
+  geoLocation: GeoLocation
 }
