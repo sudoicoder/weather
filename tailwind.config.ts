5255bd8e2f0d635076c4cfd0b51713781f2bbbca
sudoicoder <sudoicoder@gmail.com>
2023-09-26 13:02:24 +0530

    Installed shadcn

--- tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "tailwindcss"
-
-const config: Config = {
-  content: ["./src/components/**/*.tsx", "./src/app/**/*.tsx"],
-  plugins: [],
-  theme: {},
-}
-export default config
