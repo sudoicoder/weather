259ed8c25096accc22e9118440dbe754470729cf
sudoicoder <sudoicoder@gmail.com>
2023-09-26 18:15:11 +0530

    Moved GeoLocation into Weather

--- src/entities/Weather.ts
+++ src/entities/Weather.ts
@@ -1,108 +1,36 @@
-import { addDays, addHours } from "date-fns"
+interface Location {
+  latitude: number
+  longitute: number
+  name: string
+}
 
-import type { GeoLocation } from "./GeoLocation"
-import { devGeoLocation } from "./GeoLocation"
+interface Condition {
+  description: string
+  icon: string
+}
 
-export type WeatherCondition =
-  | "cloudy"
-  | "meteor"
-  | "rainy"
-  | "sunny"
-  | "thunderstorm"
-
-export interface CurrentWeather {
-  condition: WeatherCondition
+interface CurrentWeather {
+  condition: Condition
   currentTemperature: number
-  date: Date
+  timestamp: string
+}
+
+interface DailyForecast {
+  condition: Condition
+  date: string
   maximumTemperature: number
   minimumTemperature: number
 }
 
-export interface Forecast {
-  condition: WeatherCondition
-  date: Date
-  maximumTemperature: number
-  minimumTemperature: number
+interface HourlyForecast {
+  condition: Condition
+  predictedTemperature: number
+  time: string
 }
 
-export interface Weather {
+export default interface Weather {
   currentWeather: CurrentWeather
-  dailyForecasts: Forecast[]
-  geoLocation: GeoLocation
-  hourlyForecasts: Forecast[]
-}
-
-const devDate = new Date(2023, 9, 26)
-
-const devCurrentWeather: CurrentWeather = {
-  condition: "rainy",
-  currentTemperature: 30,
-  date: devDate,
-  maximumTemperature: 32,
-  minimumTemperature: 27,
-}
-
-const devDailyForecasts: Forecast[] = [
-  {
-    condition: "cloudy",
-    date: addDays(devDate, 1),
-    maximumTemperature: 33,
-    minimumTemperature: 26,
-  },
-  {
-    condition: "rainy",
-    date: addDays(devDate, 2),
-    maximumTemperature: 33,
-    minimumTemperature: 26,
-  },
-  {
-    condition: "sunny",
-    date: addDays(devDate, 3),
-    maximumTemperature: 34,
-    minimumTemperature: 26,
-  },
-  {
-    condition: "thunderstorm",
-    date: addDays(devDate, 4),
-    maximumTemperature: 25,
-    minimumTemperature: 15,
-  },
-]
-
-const devHourlyForecasts: Forecast[] = [
-  {
-    condition: "cloudy",
-    date: addHours(devDate, 1),
-    maximumTemperature: 33,
-    minimumTemperature: 26,
-  },
-  {
-    condition: "rainy",
-    date: addHours(devDate, 2),
-    maximumTemperature: 33,
-    minimumTemperature: 26,
-  },
-  {
-    condition: "sunny",
-    date: addHours(devDate, 3),
-    maximumTemperature: 34,
-    minimumTemperature: 26,
-  },
-  {
-    condition: "sunny",
-    date: addHours(devDate, 4),
-    maximumTemperature: 34,
-    minimumTemperature: 26,
-  },
-]
-
-const devWeather: Weather = {
-  currentWeather: devCurrentWeather,
-  dailyForecasts: devDailyForecasts,
-  geoLocation: devGeoLocation,
-  hourlyForecasts: devHourlyForecasts,
-}
-
-export async function getWeather() {
-  return devWeather
+  dailyForecasts: DailyForecast[]
+  hourlyForecasts: HourlyForecast[]
+  location: Location
 }
