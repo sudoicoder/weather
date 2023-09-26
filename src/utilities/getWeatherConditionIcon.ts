36127fde2864646a0aebc586a24b059f91ad09f4
sudoicoder <sudoicoder@gmail.com>
2023-09-26 18:14:27 +0530

    Deleted getWeatherConditionIcon

--- src/utilities/getWeatherConditionIcon.ts
+++
@@ -1,7 +0,0 @@
-import { WeatherCondition } from "@/entities/Weather"
-
-export default function getWeatherConditionIcon(
-  weatherCondition: WeatherCondition
-) {
-  return `/icons/${weatherCondition}.png`
-}
