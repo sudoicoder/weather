9a83d2f4e04a89667e87ed0281605b177db97b28
sudoicoder <sudoicoder@gmail.com>
2023-09-26 18:14:36 +0530

    Created getEnv

---
+++ src/utilities/getEnv.ts
@@ -0,0 +1,12 @@
+import { z } from "zod"
+
+const envSchema = z.object({
+  WEATHER_API_BASE_URL: z.string().url(),
+  WEATHER_API_KEY: z.string().nonempty(),
+})
+
+const env = envSchema.parse(process.env)
+
+export default function getEnv(key: keyof z.infer<typeof envSchema>) {
+  return env[key]
+}
