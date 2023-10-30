760859a2c64f5dcd29771cd4269cc74cd10715fd
sudoicoder <sudoicoder@gmail.com>
2023-10-30 16:18:28 +0530

    app/page: redirects to /latitude/[latitude]/longitude/[longitude]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
-import { DailyForecast } from "@/components/DailyForecast"
-import { HourlyForecast } from "@/components/HourlyForecast"
-import { WeatherCard } from "@/components/WeatherCard"
+import { redirect } from "next/navigation"
 
-import { getWeather } from "@/services/getWeather"
+import { defaultGeocoordinates } from "@/entities/Geocoordinates"
 
-export default async function Home() {
-  const weather = await getWeather({ latitude: 11.9416, longitude: 79.8083 })
-  return (
-    <main className="flex flex-col items-center gap-5 my-5">
-      <div className="text-3xl">{weather.location.name}</div>
-      <WeatherCard currentWeather={weather.currentWeather} />
-      <div className="max-w-md">
-        <HourlyForecast hourlyForecasts={weather.hourlyForecasts} />
-      </div>
-      <DailyForecast dailyForecasts={weather.dailyForecasts} />
-    </main>
-  )
+const { latitude: defaultLatitude, longitude: defaultLongitude } =
+  defaultGeocoordinates
+
+export default function Home() {
+  return redirect(`/latitude/${defaultLatitude}/longitude/${defaultLongitude}`)
 }
