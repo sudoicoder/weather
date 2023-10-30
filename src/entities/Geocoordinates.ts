147feaeaf25d78ad9ce51c1ce65c860dedf9c71b
sudoicoder <sudoicoder@gmail.com>
2023-10-30 16:17:50 +0530

    Geocoordinates: exports defaultGeocoordinates

--- src/entities/Geocoordinates.ts
+++ src/entities/Geocoordinates.ts
@@ -1,8 +1,13 @@
 import * as zod from "zod"
 
 export const GeocoordinatesSchema = zod.object({
-  latitude: zod.number().default(0),
-  longitude: zod.number().default(0),
+  latitude: zod.number(),
+  longitude: zod.number(),
 })
 
 export type Geocoordinates = zod.infer<typeof GeocoordinatesSchema>
+
+export const defaultGeocoordinates: Geocoordinates = {
+  latitude: 11.9416,
+  longitude: 79.8083,
+}
