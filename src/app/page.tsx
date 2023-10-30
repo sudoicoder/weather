b762555200c215120a2025cd2cef52d2fdbf9260
sudoicoder <sudoicoder@gmail.com>
2023-10-30 11:46:04 +0530

    Uninstalled shadcn/select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import WeatherCard from "@/components/WeatherCard"
 import getWeather from "@/services/getWeather"
 
 export default async function Home() {
-  const weather = await getWeather()
+  const weather = await getWeather({ latitude: 11.9416, longitude: 79.8083 })
   return (
     <main className="flex flex-col items-center gap-5 my-5">
       <div className="text-3xl">{weather.location.name}</div>
